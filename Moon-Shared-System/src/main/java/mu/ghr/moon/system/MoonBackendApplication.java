96e61f6adcf442c61e5a23e9bf1a627bcdd95794
ganesh <heerahganesh@gmail.com>
2020-08-04 21:43:23 +0400

    Intial-commit-ghr01

---
+++ Moon-Shared-System/src/main/java/mu/ghr/moon/system/MoonBackendApplication.java
@@ -0,0 +1,13 @@
+package mu.ghr.moon.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonBackendApplication.class, args);
+    }
+
+}
